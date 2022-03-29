b4aa623f745fab229b58a012a588129749f05170
Sergio Cano <canosergio90@gmail.com>
2022-03-29 16:14:47 -0300

    AV
    Signed-off-by: Sergio Cano <canosergio90@gmail.com>

--- follower-service/src/main/scala/com/soundcloud/maze/adapters/in/messagebroker/BrokerConsumer.scala
+++ follower-service/src/main/scala/com/soundcloud/maze/adapters/in/messagebroker/BrokerConsumer.scala
@@ -3,11 +3,27 @@ package com.soundcloud.maze.adapters.in.messagebroker
 import fs2.kafka.ConsumerSettings
 import fs2.kafka.KafkaConsumer
 import cats.effect.IO
+import fs2.kafka.ConsumerRecord
+import com.soundcloud.maze.domain.entities.Event
 
 object BrokerConsumer {
 
-  type RecordHandler[T] = String => IO[T]
+  type RecordHandler[R, T] = R => IO[T]
 
-  def startConsumer(handler: RecordHandler[Unit], settings: ConsumerSettings[IO, String, String]) =
-    KafkaConsumer.stream(settings)
+  type EventParser = String => Event
+
+  def consumer(
+      handler: RecordHandler[Event, Unit],
+      eventParser: EventParser,
+      topic: String,
+      settings: ConsumerSettings[IO, String, String]
+  ) =
+    KafkaConsumer
+      .stream(settings)
+      .subscribeTo(topic)
+      .records
+      .mapAsync(4) { commitable =>
+        val event = eventParser(commitable.record.value)
+        handler(event)
+      }
 }
