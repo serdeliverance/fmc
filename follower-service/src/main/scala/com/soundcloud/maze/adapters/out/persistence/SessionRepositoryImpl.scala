7be654d043c9b4dcf656847ea1752847c25aac60
Sergio Cano <canosergio90@gmail.com>
2022-03-30 11:35:40 -0300

    av
    Signed-off-by: Sergio Cano <canosergio90@gmail.com>

--- follower-service/src/main/scala/com/soundcloud/maze/adapters/out/persistence/SessionRepositoryImpl.scala
+++ follower-service/src/main/scala/com/soundcloud/maze/adapters/out/persistence/SessionRepositoryImpl.scala
@@ -3,10 +3,18 @@ package com.soundcloud.maze.adapters.out.persistence
 import com.soundcloud.maze.application.ports.out.SessionRepository
 import com.soundcloud.maze.domain.entities.Session
 import cats.effect.IO
+import cats.effect.kernel.Resource
+import dev.profunktor.redis4cats.RedisCommands
 
-class SessionServiceImpl extends SessionRepository {
+class SessionServiceImpl(redisCommand: Resource[IO, RedisCommands[IO, String, String]]) extends SessionRepository {
   // TODO inmemory session repository using CE data structures
-  override def create(session: Session): IO[Unit]             = ???
+  override def create(session: Session): IO[Unit] =
+    redisCommand.use { redis =>
+      for {
+        // TODO autogenerate session ids
+        _ <- redis.hSet("session-id", session.toMap)
+      } yield ()
+    }
   override def delete(sessionId: Long): IO[Unit]              = ???
   override def getByUserId(userId: Long): IO[Option[Session]] = ???
 }
