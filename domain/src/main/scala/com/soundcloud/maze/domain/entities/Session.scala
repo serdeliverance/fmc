b3f0e5ce2fb9555682c4da4ede48ed6c71c6f6bd
Sergio Cano <canosergio90@gmail.com>
2022-03-30 18:57:14 -0300

    refactoring session data type
    Signed-off-by: Sergio Cano <canosergio90@gmail.com>

--- domain/src/main/scala/com/soundcloud/maze/domain/entities/Session.scala
+++ domain/src/main/scala/com/soundcloud/maze/domain/entities/Session.scala
@@ -1,7 +1,16 @@
 package com.soundcloud.maze.domain.entities
 
 import java.util.UUID
+import Session._
 
-case class Session(id: Option[UUID], userId: Long, address: Address)
+case class Session private (id: SessionId, userId: Long, address: Address)
 
 case class Address(ip: String, port: Int)
+
+object Session {
+
+  type SessionId = String
+
+  def apply(userId: Long, address: Address): Session =
+    Session(s"user-$userId", userId, address)
+}
