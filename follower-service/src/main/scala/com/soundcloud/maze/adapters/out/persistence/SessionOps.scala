e554b4657bffa2c3a3b08c607a07eb896c1fa9a1
Sergio Cano <canosergio90@gmail.com>
2022-03-30 14:45:15 -0300

    av
    Signed-off-by: Sergio Cano <canosergio90@gmail.com>

--- follower-service/src/main/scala/com/soundcloud/maze/adapters/out/persistence/SessionOps.scala
+++ follower-service/src/main/scala/com/soundcloud/maze/adapters/out/persistence/SessionOps.scala
@@ -1,10 +1,10 @@
-package com.soundcloud.maze.adapters.out.persistence.SessionOps
+package com.soundcloud.maze.adapters.out.persistence
 
 import com.soundcloud.maze.domain.entities.Session
 
 object SessionOps {
 
-  implicit class SessionOps(session: Session) {
+  implicit class SessionToMapConverter(session: Session) {
     def toMap: Map[String, String] =
       // TODO extract fields to constants
       Map("userId" -> session.userId.toString, "address.ip" -> session.address.ip, "address.port" -> session.address.port.toString)
--- follower-service/src/main/scala/com/soundcloud/maze/adapters/out/persistence/SessionRepositoryImpl.scala
+++ follower-service/src/main/scala/com/soundcloud/maze/adapters/out/persistence/SessionRepositoryImpl.scala
@@ -1,6 +1,7 @@
 package com.soundcloud.maze.adapters.out.persistence
 
 import com.soundcloud.maze.application.ports.out.SessionRepository
+import com.soundcloud.maze.adapters.out.persistence.SessionOps.SessionToMapConverter
 import com.soundcloud.maze.domain.entities.Session
 import cats.effect.IO
 import cats.effect.kernel.Resource
@@ -8,12 +9,11 @@ import dev.profunktor.redis4cats.RedisCommands
 import java.util.UUID
 
 class SessionServiceImpl(redisCommand: Resource[IO, RedisCommands[IO, String, String]]) extends SessionRepository {
-  // TODO inmemory session repository using CE data structures
   override def create(session: Session): IO[Session] =
     redisCommand.use { redis =>
       for {
         uuid <- IO.pure(UUID.randomUUID())
-        _    <- redis.hSet(uuid, session.toMap)
+        _    <- redis.hSet(uuid.toString, session.toMap)
       } yield (session.copy(id = Some(uuid)))
     }
   override def delete(sessionId: Long): IO[Unit]              = ???
